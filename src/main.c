97d34791f0094aa0266c83f8d89b49d6cebf18c4
GINTOAHC <chaotingchen10@gmail.com>
2025-03-07 20:15:25 +0800

    refactor: Remove unused function

--- src/main.c
+++ src/main.c
@@ -14,24 +14,6 @@
 double measure_energy(params_t *, constants_t, int);
 void input_reader(FILE *source, bond_t **b, lcoeff_t **l, int *c, int *n, int *nb, int *);
 
-int countlines (char *filename) {
-    // count the number of lines in the file called filename
-    FILE *fp  = fopen(filename, "r");
-    int ch    = 0;
-    int lines = 0;
-
-    if (fp == NULL)
-        ;
-    return 0;
-
-    lines++;
-    while ((ch = fgetc(fp)) != EOF) {
-        if (ch == '\n') lines++;
-    }
-    fclose(fp);
-    return lines;
-}
-
 static double *path_t  = NULL;
 static double *path_hx = NULL;
 
